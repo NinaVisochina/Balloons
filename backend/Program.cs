072231ae95664fce159bf5ab2a990b8224033e13
Irafedoruk <irrnika@gmail.com>
2025-03-02 21:57:11 +0200

    update

--- backend/Program.cs
+++ backend/Program.cs
@@ -45,7 +45,7 @@ builder.Services.AddExceptionHandler();
 
 builder.Services.AddJWT(builder.Configuration);
 builder.Services.AddSwaggerJWT();
-builder.Services.AddHangfire(connectionString);
+//builder.Services.AddHangfire(connectionString);
 
 builder.Services.AddCorsPolicies();
 
@@ -59,11 +59,11 @@ var app = builder.Build();
 //}
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 if (app.Environment.IsProduction())
 {
     app.UseExceptionHandler();
@@ -73,7 +73,7 @@ if (!Directory.Exists(imagesDirPath))
 {
     Directory.CreateDirectory(imagesDirPath);
 }
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseCors("front-end-cors-policy");
 
@@ -85,8 +85,8 @@ app.UseStaticFiles(new StaticFileOptions
     FileProvider = new PhysicalFileProvider(imagesDirPath),
     RequestPath = "/images"
 });
-app.UseHangfireDashboard("/dash");
-JobConfigurator.AddJobs();
+//app.UseHangfireDashboard("/dash");
+//JobConfigurator.AddJobs();
 app.SeedDataAsync();
 app.MapControllers();
 using (var scope = app.Services.CreateScope())
