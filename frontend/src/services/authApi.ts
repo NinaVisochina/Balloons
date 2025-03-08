2a91ad15a9400ca5f084c3c713ae03f0d75df8b7
Irafedoruk <irrnika@gmail.com>
2025-03-08 15:53:14 +0200

    Is good app Login

--- frontend/src/services/authApi.ts
+++ frontend/src/services/authApi.ts
@@ -3,9 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_API_URL
-      ? `${import.meta.env.VITE_API_URL}/api`
-      : "/api",
+    baseUrl: `${import.meta.env.VITE_API_URL}/api/accounts/auth`,
   }),
   endpoints: (builder) => ({
     login: builder.mutation<
