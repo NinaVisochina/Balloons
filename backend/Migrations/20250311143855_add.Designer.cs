16e086afc64fd4e026a6dadeb727164ccd35eeff
NinaVisochina <114816977+NinaVisochina@users.noreply.github.com>
2025-03-11 16:40:46 +0200

    add baza

--- backend/Migrations/20250311143855_add.Designer.cs
+++ backend/Migrations/20250311143855_add.Designer.cs
@@ -12,7 +12,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BackendShop.Migrations
 {
     [DbContext(typeof(ShopDbContext))]
-    [Migration("20250310154501_add")]
+    [Migration("20250311143855_add")]
     partial class add
     {
         /// <inheritdoc />
