d0ab5424dc69e7b8a18a436beb20334c6af96d45
NinaVisochina <114816977+NinaVisochina@users.noreply.github.com>
2025-03-11 17:15:00 +0200

    add baza5

--- backend/Migrations/20250311150640_add2.cs
+++ backend/Migrations/20250311150640_add2.cs
@@ -7,52 +7,52 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BackendShop.Migrations
 {
     /// <inheritdoc />
-    public partial class add : Migration
+    public partial class add2 : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-                name: "AspNetRoles",
-                columns: table => new
-                {
-                    Id = table.Column<string>(type: "text", nullable: false),
-                    Name = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
-                    NormalizedName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
-                    ConcurrencyStamp = table.Column<string>(type: "text", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_AspNetRoles", x => x.Id);
-                });
+            //migrationBuilder.CreateTable(
+            //    name: "AspNetRoles",
+            //    columns: table => new
+            //    {
+            //        Id = table.Column<string>(type: "text", nullable: false),
+            //        Name = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+            //        NormalizedName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+            //        ConcurrencyStamp = table.Column<string>(type: "text", nullable: true)
+            //    },
+            //    constraints: table =>
+            //    {
+            //        table.PrimaryKey("PK_AspNetRoles", x => x.Id);
+            //    });
 
-            migrationBuilder.CreateTable(
-                name: "AspNetUsers",
-                columns: table => new
-                {
-                    Id = table.Column<string>(type: "text", nullable: false),
-                    Firstname = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
-                    Lastname = table.Column<string>(type: "text", nullable: true),
-                    Birthdate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
-                    UserName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
-                    NormalizedUserName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
-                    Email = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
-                    NormalizedEmail = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
-                    EmailConfirmed = table.Column<bool>(type: "boolean", nullable: false),
-                    PasswordHash = table.Column<string>(type: "text", nullable: true),
-                    SecurityStamp = table.Column<string>(type: "text", nullable: true),
-                    ConcurrencyStamp = table.Column<string>(type: "text", nullable: true),
-                    PhoneNumber = table.Column<string>(type: "text", nullable: true),
-                    PhoneNumberConfirmed = table.Column<bool>(type: "boolean", nullable: false),
-                    TwoFactorEnabled = table.Column<bool>(type: "boolean", nullable: false),
-                    LockoutEnd = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
-                    LockoutEnabled = table.Column<bool>(type: "boolean", nullable: false),
-                    AccessFailedCount = table.Column<int>(type: "integer", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_AspNetUsers", x => x.Id);
-                });
+            //migrationBuilder.CreateTable(
+            //    name: "AspNetUsers",
+            //    columns: table => new
+            //    {
+            //        Id = table.Column<string>(type: "text", nullable: false),
+            //        Firstname = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+            //        Lastname = table.Column<string>(type: "text", nullable: true),
+            //        Birthdate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+            //        UserName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+            //        NormalizedUserName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+            //        Email = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+            //        NormalizedEmail = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+            //        EmailConfirmed = table.Column<bool>(type: "boolean", nullable: false),
+            //        PasswordHash = table.Column<string>(type: "text", nullable: true),
+            //        SecurityStamp = table.Column<string>(type: "text", nullable: true),
+            //        ConcurrencyStamp = table.Column<string>(type: "text", nullable: true),
+            //        PhoneNumber = table.Column<string>(type: "text", nullable: true),
+            //        PhoneNumberConfirmed = table.Column<bool>(type: "boolean", nullable: false),
+            //        TwoFactorEnabled = table.Column<bool>(type: "boolean", nullable: false),
+            //        LockoutEnd = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+            //        LockoutEnabled = table.Column<bool>(type: "boolean", nullable: false),
+            //        AccessFailedCount = table.Column<int>(type: "integer", nullable: false)
+            //    },
+            //    constraints: table =>
+            //    {
+            //        table.PrimaryKey("PK_AspNetUsers", x => x.Id);
+            //    });
 
             migrationBuilder.CreateTable(
                 name: "Discounts",
