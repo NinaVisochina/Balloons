07fc1bb5393188d25a22d81af60c6b62dc63f738
Irafedoruk <irrnika@gmail.com>
2025-03-12 17:34:21 +0200

    fix registration

--- frontend/src/components/client/auth/RegisterPage.tsx
+++ frontend/src/components/client/auth/RegisterPage.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { API_URL } from "../../../env";
 
 const RegisterPage = () => {
     const [email, setEmail] = useState("");
@@ -14,7 +15,7 @@ const RegisterPage = () => {
       }
   
       try {
-        const response = await fetch("/api/accounts/auth/register", {
+        const response = await fetch(`${API_URL}/api/accounts/auth/register`, {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ email, password }),
