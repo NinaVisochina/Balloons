5a97db00563aee4c7df6460d6973c8c694f7b40e
NinaVisochina <114816977+NinaVisochina@users.noreply.github.com>
2025-03-27 14:59:22 +0200

    searsh

--- frontend/src/components/client/layout/ClientLayout.tsx
+++ frontend/src/components/client/layout/ClientLayout.tsx
@@ -25,12 +25,12 @@ const ClientLayout = () => {
   const { data: categories, isLoading: categoriesLoading } = useGetCategoriesQuery();
 
   const categorySlug = categories?.find(cat => cat.id === hoveredCategory)?.slug ?? '';
-  
+
   const { data: subCategoryData } = useGetSubCategoriesByCategorySlugQuery(
     categorySlug,
     { skip: !hoveredCategory || !categories }
   );
-  
+
   useEffect(() => {
     if (subCategoryData && hoveredCategory !== null) {
       const filtered = subCategoryData.filter(
@@ -39,20 +39,30 @@ const ClientLayout = () => {
       setFilteredSubCategories(filtered);
     }
   }, [subCategoryData, hoveredCategory]);
-  
 
+
+  // const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   const value = e.target.value;
+  //   console.log("🔍 Введений пошуковий запит:", value);
+  //   setSearch(value);
+  //   setShowSuggestions(value.length >= 3);
+  // };
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
-    console.log("🔍 Введений пошуковий запит:", value);
     setSearch(value);
-    setShowSuggestions(value.length >= 3);
-};
-
+    setShowSuggestions(value.trim().length >= 3);
+  };
+  
+  // const handleSuggestionClick = (productId: number) => {
+  //   navigate(`/product/${productId}`);
+  //   setShowSuggestions(false);
+  // };
   const handleSuggestionClick = (productId: number) => {
     navigate(`/product/${productId}`);
     setShowSuggestions(false);
+    setSearch(""); // Очищаємо поле після переходу
   };
-    
+  
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -81,15 +91,15 @@ const ClientLayout = () => {
   const handleSearchSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     if (search.trim()) {
-        console.log("📡 Відправка запиту на API:", search);
-        navigate(`/product/${searchResults?.[0]?.id}`);
-        setShowSuggestions(false);
+      console.log("📡 Відправка запиту на API:", search);
+      navigate(`/product/${searchResults?.[0]?.id}`);
+      setShowSuggestions(false);
     } else {
-        console.warn("⚠️ Порожній запит не відправляється!");
+      console.warn("⚠️ Порожній запит не відправляється!");
     }
-};
+  };
 
-  
+
 
   const navigate = useNavigate();
 
@@ -104,7 +114,7 @@ const ClientLayout = () => {
               <span className="text-pink-500">Shop</span>
             </span>
           </Link>
-  
+
           <button
             onClick={toggleMenu}
             className="bg-gradient-to-r from-accent to-pink-500 text-white font-sans px-6 py-2 sm:px-4 sm:py-1 xs:px-3 xs:py-1 rounded-lg shadow-md hover:from-accentDark hover:to-pink-600 hover:shadow-lg transition duration-300"
@@ -112,62 +122,61 @@ const ClientLayout = () => {
             Каталог
           </button>
           {isMenuOpen && !categoriesLoading && categories && (
-  <div
-    className="absolute left-0 top-0 w-[500px] h-screen bg-white text-text shadow-lg z-50 flex"
-    onMouseEnter={() => setIsMenuOpen(true)}
-    onMouseLeave={() => setIsMenuOpen(false)}
-  >
-    {/* Ліва панель категорій */}
-    <div className="w-[250px] bg-gray-50 p-4">
-      <ul className="space-y-2">
-        {categories.map((category) => (
-          <li
-            key={category.id}
-            className={`flex justify-between items-center px-4 py-2 cursor-pointer transition-all duration-300 ${
-              hoveredCategory === category.id ? "bg-accent text-white" : "hover:bg-gray-200"
-            }`}
-            onMouseEnter={() => handleCategoryHover(category.id)}
-            onClick={() => {
-              navigate(`/category/${category.slug}`);
-              setIsMenuOpen(false); // Закриває меню після натискання
-            }}
-          >
-            <span>{category.name}</span>
-            <span className="text-secondary">›</span>
-          </li>
-        ))}
-      </ul>
-    </div>
+            <div
+              className="absolute left-0 top-0 w-[500px] h-screen bg-white text-text shadow-lg z-50 flex"
+              onMouseEnter={() => setIsMenuOpen(true)}
+              onMouseLeave={() => setIsMenuOpen(false)}
+            >
+              {/* Ліва панель категорій */}
+              <div className="w-[250px] bg-gray-50 p-4">
+                <ul className="space-y-2">
+                  {categories.map((category) => (
+                    <li
+                      key={category.id}
+                      className={`flex justify-between items-center px-4 py-2 cursor-pointer transition-all duration-300 ${hoveredCategory === category.id ? "bg-accent text-white" : "hover:bg-gray-200"
+                        }`}
+                      onMouseEnter={() => handleCategoryHover(category.id)}
+                      onClick={() => {
+                        navigate(`/category/${category.slug}`);
+                        setIsMenuOpen(false); // Закриває меню після натискання
+                      }}
+                    >
+                      <span>{category.name}</span>
+                      <span className="text-secondary">›</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
 
-    {/* Права панель підкатегорій (при наведенні на категорію) */}
-    {hoveredCategory && filteredSubCategories.length > 0 && (
-      <div
-        className="w-[250px] bg-white shadow-md p-4 border-l border-gray-200 transition-opacity duration-300"
-        onMouseEnter={() => setHoveredCategory(hoveredCategory)}
-        onMouseLeave={() => setHoveredCategory(null)}
-      >
-        <h3 className="font-caveat text-text text-lg mb-2">Підкатегорії</h3>
-        <ul className="space-y-2">
-          {filteredSubCategories.map((subCategory) => (
-            <li key={subCategory.id}>
-              <Link
-                to={`/subcategory/products/${subCategory.slug}`}
-                className="text-text hover:text-accent transition-all"
-                onClick={() => {
-                  setIsMenuOpen(false); // Закриває меню після натискання
-                }}
-              >
-                {subCategory.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    )}
-  </div>
-)}
+              {/* Права панель підкатегорій (при наведенні на категорію) */}
+              {hoveredCategory && filteredSubCategories.length > 0 && (
+                <div
+                  className="w-[250px] bg-white shadow-md p-4 border-l border-gray-200 transition-opacity duration-300"
+                  onMouseEnter={() => setHoveredCategory(hoveredCategory)}
+                  onMouseLeave={() => setHoveredCategory(null)}
+                >
+                  <h3 className="font-caveat text-text text-lg mb-2">Підкатегорії</h3>
+                  <ul className="space-y-2">
+                    {filteredSubCategories.map((subCategory) => (
+                      <li key={subCategory.id}>
+                        <Link
+                          to={`/subcategory/products/${subCategory.slug}`}
+                          className="text-text hover:text-accent transition-all"
+                          onClick={() => {
+                            setIsMenuOpen(false); // Закриває меню після натискання
+                          }}
+                        >
+                          {subCategory.name}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </div>
+          )}
 
-          <form onSubmit={handleSearchSubmit} className="flex items-center bg-white rounded-lg px-4 py-2 sm:px-3 sm:py-1 xs:px-2 xs:py-1 border border-gray-200">
+          {/* <form onSubmit={handleSearchSubmit} className="flex items-center bg-white rounded-lg px-4 py-2 sm:px-3 sm:py-1 xs:px-2 xs:py-1 border border-gray-200">
             <input
               type="text"
               placeholder="Я шукаю..."
@@ -194,8 +203,41 @@ const ClientLayout = () => {
                 ))}
               </ul>
             )}
-          </form>
-  
+          </form> */}
+          <div className="relative w-64 sm:w-48 xs:w-32">
+            <form onSubmit={handleSearchSubmit} className="flex items-center bg-white rounded-lg px-4 py-2 sm:px-3 sm:py-1 xs:px-2 xs:py-1 border border-gray-200">
+              <input
+                type="text"
+                placeholder="Я шукаю..."
+                className="outline-none px-3 py-1 text-lg sm:text-base xs:text-sm w-full"
+                value={search}
+                onChange={handleSearch}
+              />
+              <button
+                type="submit"
+                className="p-2 rounded-full bg-gradient-to-r from-accent to-pink-500 text-white hover:from-accentDark hover:to-pink-600 transition duration-300 shadow-md hover:shadow-lg"
+              >
+                <FaSearch size={18} className="sm:w-4 sm:h-4 xs:w-3 xs:h-3" />
+              </button>
+            </form>
+
+            {/* Підказки */}
+            {showSuggestions && searchResults && searchResults.length > 0 && (
+              <ul className="absolute top-full left-0 z-50 w-full bg-white shadow-md rounded-md mt-1 max-h-60 overflow-y-auto border border-gray-200">
+                {searchResults.map((product) => (
+                  <li
+                    key={product.id}
+                    className="px-4 py-2 hover:bg-gray-100 cursor-pointer text-text"
+                    onClick={() => handleSuggestionClick(product.id)}
+                  >
+                    {product.name}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+
+
           <div className="flex items-center space-x-6 sm:space-x-4 xs:space-x-2">
             <Link to="/cart" className="relative flex items-center">
               <div className="p-2 rounded-full bg-gradient-to-r from-accent to-pink-500 text-white hover:from-accentDark hover:to-pink-600 transition duration-300 shadow-md hover:shadow-lg">
@@ -231,14 +273,14 @@ const ClientLayout = () => {
         </div>
       </header>
       {/* className=container mx-auto py-6 px-6 */}
-      <main className="max-w-7xl mx-auto py-6 px-6">  
+      <main className="max-w-7xl mx-auto py-6 px-6">
         <Outlet />
       </main>
-  
+
       <Footer />
     </div>
   );
-  
+
 };
 
 export default ClientLayout;
